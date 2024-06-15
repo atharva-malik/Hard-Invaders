b037b3aa695818f6e301d25d229dc14922cae942
Atharva <atharva.malik@gmail.com>
2024-06-15 20:14:29 +1000

    Organisation

---
+++ SpaceInvaders.py
@@ -0,0 +1,45 @@
+import pygame
+
+class Game:
+    def __init__(self):
+        pass
+
+class Spaceship(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+    
+
+class Fortification(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+    
+
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+    
+
+class Common_Enemy(Enemy):
+    def __init__(self):
+        super().__init__()
+    
+
+class Rare_Enemy(Enemy):
+    def __init__(self):
+        super().__init__()
+    
+
+class Epic_Enemy(Enemy):
+    def __init__(self):
+        super().__init__()
+    
+
+class Mythic_Enemy(Enemy):
+    def __init__(self):
+        super().__init__()
+    
+
+class Legendary_Enemy(Enemy):
+    def __init__(self):
+        super().__init__()
+    
